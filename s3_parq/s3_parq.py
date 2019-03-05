1871995c226d3f748f92d21f46aebe9259fe595c
Ethan Knox <emk@integrichain.com>
2019-03-05 09:26:45 -0500

    refactor for clarity

--- s3_parq/s3_parq.py
+++ s3_parq/s3_parq.py
@@ -1,32 +1,97 @@
 import boto3
 from .s3_parq_publish import S3ParqPublish
 import pandas as pd
+import sys
+
 
 class S3Parq:
 
     def __init__(self, **kwargs):
         self._set_kwargs_as_attrs(**kwargs)
-        
 
-    def publish(self,**kwargs)->None:
-        for required_attr in ('dataset','bucket','dataframe'):
-            if not hasattr(self,required_attr):
-                raise TypeError(f"Unable to call S3Parq.publish(); missing required attribute {required_attr}")
-    
+    def publish(self)->None:
+        required_attributes = ('dataset', 'bucket', 'dataframe',)
+        self._check_required_attr(required_attributes)
+
         publish = S3ParqPublish(dataset=self.dataset,
                                 bucket=self.bucket,
                                 key_prefix=self.key_prefix,
                                 dataframe=self.dataframe)
         return publish.do_publish()
 
-    def _set_kwargs_as_attrs(self, **kwargs)->None:       
-        for k in [  ('dataset',str,),
-                    ('bucket',str,),
-                    ('key_prefix',str,),
-                    ('dataframe',pd.DataFrame,),
-                    ('filters',dict,)]:
-            if k[0] in kwargs.keys():
-                key, typeof = k
-                if not isinstance(kwargs[key],typeof):
-                    raise TypeError(f"Bad value for {key}; {kwargs[key]} is not an instance of {typeof}")
+    def fetch(self)->None:
+        required_attributes = ('dataset', 'bucket',)
+        self._check_required_attr(required_attributes)
+        # DO fetch here
+
+    @property
+    def dataset(self)->str:
+        return self._dataset
+
+    @dataset.setter
+    def dataset(self, dataset: str)->None:
+        self._type_check_attr('dataset', dataset)
+        self._dataset = dataset
+
+    @property
+    def bucket(self)->str:
+        return self._bucket
+
+    @bucket.setter
+    def bucket(self, bucket: str)->None:
+        self._type_check_attr('bucket', bucket)
+        self._bucket = bucket
+
+    @property
+    def dataframe(self)->pd.DataFrame:
+        return self._dataframe
+
+    @dataframe.setter
+    def dataframe(self, dataframe: pd.DataFrame)->None:
+        self._type_check_attr('dataframe', dataframe)
+        self._dataframe = dataframe
+
+    @property
+    def filters(self)->dict:
+        return self._filters
+
+    @filters.setter
+    def filters(self, filters: dict)->None:
+        self._type_check_attr('filters', filters)
+        self._filters = filters
+
+    @property
+    def key_prefix(self)->str:
+        return self._key_prefix
+
+    @key_prefix.setter
+    def key_prefix(self, key_prefix: str)->None:
+        self._type_check_attr('key_prefix', key_prefix)
+        self._key_prefix = key_prefix
+
+    def _check_required_attr(self, attributes: iter)->None:
+        """ make sure all required attributes are set before running.
+            The sys._getframe bit is the name of the calling function (in this case S3Parq.fetch / S3Parq.publish).
+        """
+        for required_attr in attributes:
+            if not hasattr(self, required_attr):
+                raise TypeError(
+                    f"Unable to call S3Parq.{sys._getframe(1).f_code.co_name}; missing required attribute {required_attr}")
+
+    def _type_check_attr(self, attr: str, value)->None:
+        """ checks typing of attribute and throws error if it is incorrect."""
+        for k in [('dataset', str,),
+                  ('bucket', str,),
+                  ('key_prefix', str,),
+                  ('dataframe', pd.DataFrame,),
+                  ('filters', dict,)]:
+        if attr == k[0]:
+            if not isinstance(value, k[1]):
+                raise TypeError(
+                    f"Bad value for {attr}; {value} is not an instance of {k[1]}")
+
+    def _set_kwargs_as_attrs(self, **kwargs)->None:
+        """ type check and set instance attributes."""
+        for key in kwargs.keys():
+                self._type_check_attr(key, kwargs[key])
                 self.__dict__[key] = kwargs[key]
--- s3_parq/s3_parq_publish.py
+++ s3_parq/s3_parq_publish.py
@@ -1,12 +1,14 @@
 import boto3
 import pandas as pd
 from collections import namedtuple
+
+
 class S3ParqPublish:
 
-    def __init__(self, dataset:str, dataframe:pd.DataFrame, bucket:str, key_prefix:str='')->None:   
+    def __init__(self, dataset: str, dataframe: pd.DataFrame, bucket: str, key_prefix: str = '')->None:
         pass
 
     def do_publish(self):
-        response = namedtouple("response", ["rows","files"])
+        response = namedtouple("response", ["rows", "files"])
         if True:
-            return response._make(45,50)
+            return response._make(45, 50)
