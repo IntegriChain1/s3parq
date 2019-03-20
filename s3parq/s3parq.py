9444b66adc6c3680278592e42836c538c5923a77
Rayne N. Schaffer <rns@integrichain.com>
2019-03-20 14:30:56 -0400

    Added fetch_max function and fixed a fetch bug

--- s3parq/s3parq.py
+++ s3parq/s3parq.py
@@ -1,6 +1,6 @@
 import boto3
 from s3parq.publish_parq import publish
-from s3parq.fetch_parq import fetch
+from s3parq.fetch_parq import fetch, fetch_max_partition_value
 
 import pandas as pd
 import sys
@@ -15,6 +15,7 @@ class S3Parq:
                 key: str,
                 dataframe: pd.DataFrame,
                 partitions: Iterable[str]) -> None:
+
         pub = publish(
             dataframe=dataframe,
             bucket=bucket,
@@ -23,11 +24,20 @@ class S3Parq:
         )
         pub.publish()
 
-    def fetch(bucket: str,
+    def fetch(self,
+              bucket: str,
               key: str,
               **kwargs
               ) -> None:
+
         return fetch(key=key,
                      bucket=bucket,
                      filters=kwargs.get('partitions', dict())
                      )
+
+    def fetch_max_partition_value(self, bucket: str, key: str, partition: str) -> any:
+        return fetch_max_partition_value(
+            bucket=bucket,
+            key=key,
+            partition=partition
+        )
