582529e4e93131b68b14132738f1f877f5491d30
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-07-02 16:38:03 -0400

    started writing tests for schema creator

--- s3parq/schema_creator.py
+++ s3parq/schema_creator.py
@@ -6,3 +6,12 @@ def schema_string_validator(schema_query: str):
 
 def schema_generator(verified_string: str):
     return "djklfjs"
+
+from session_helper import SessionHelper
+from sqlalchemy import Column, Integer, String
+
+SH = SessionHelper('us-east-1', 'core-sandbox-cluster-1', 'core-sandbox-cluster-1.c3swieqn0nz0.us-east-1.redshift.amazonaws.com','5439','ichain_core')
+SH.configure_session_helper()
+
+with SH.db_session_scope() as scope:
+    scope.execute('CREATE SCHEMA IF NOT EXISTS justins_schema')
--- tests/test_schema_creator.py
+++ tests/test_schema_creator.py
@@ -1,9 +1,15 @@
 import pytest
 import boto3
 import s3parq.schema_creator as SC
+from moto import mock_redshift
 
 
-# Given a correctly formatted string make sure output is correct SQL
-def test_sql_output():
-    verified_string = "my_string"
-    assert SC.schema_generator(verified_string) == f"create schema if not exists {verified_string};"
+@mock_redshift
+class Test():
+    # Given a correctly formatted string make sure output is correct SQL
+    def test_sql_output():
+        verified_string = "my_string"
+        assert SC.schema_generator(verified_string) == f"create schema if not exists {verified_string};"
+
+    # Test that the function is called with the schema name
+    def test
