0b1a32bb5335203f90161ac34c401079ad36992e
Justin T. Lewis <lewisjt@JTL-MAC.local>
2019-07-03 12:01:02 -0400

    set up tests even better

--- s3parq/schema_creator.py
+++ s3parq/schema_creator.py
@@ -2,12 +2,6 @@ from s3parq.session_helper import SessionHelper
 from sqlalchemy import Column, Integer, String
 from s3parq.redshift_naming_helper import RedshiftNamingHelper
 
-
-# SH = SessionHelper('us-east-1', 'core-sandbox-cluster-1', 'core-sandbox-cluster-1.c3sw ieqn0nz0.us-east-1.redshift.amazonaws.com','5439','ichain_core')
-
-# SH.configure_session_helper()
-
-
 def schema_generator(schema_name: str):
     name_validated = RedshiftNamingHelper().validate_name(schema_name)
     if name_validated[0]:
@@ -16,7 +10,7 @@ def schema_generator(schema_name: str):
         raise ValueError(name_validated[1])
 
 
-def create_schema(schema_name: str):
+def create_schema(schema_name: str, session_helper: SessionHelper):
     schema_n = schema_generator(schema_name)
-    with self.SH.db_session_scope() as scope:
+    with session_helper.db_session_scope() as scope:
         scope.execute(f'CREATE SCHEMA IF NOT EXISTS {schema_n}')
