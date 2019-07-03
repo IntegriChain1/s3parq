2ca001d0707cdb0f00918c6204712cf15ed7f467
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-07-03 10:24:52 -0400

    Started building schema creator

--- tests/test_schema_creator.py
+++ tests/test_schema_creator.py
@@ -1,15 +1,31 @@
 import pytest
 import boto3
-import s3parq.schema_creator as SC
+from s3parq.session_helper import SessionHelper
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from moto import mock_redshift
 
 
 @mock_redshift
 class Test():
+
+    SH = SessionHelper(
+        'us-east-1',
+        'core-sandbox-cluster-1',
+        'core-sandbox-cluster-1.c3swieqn0nz0.us-east-1.redshift.amazonaws.com',
+        '5439',
+        'ichain_core'
+    )
+
+    SH.configure_session_helper()
+
     # Given a correctly formatted string make sure output is correct SQL
     def test_sql_output():
         verified_string = "my_string"
         assert SC.schema_generator(verified_string) == f"create schema if not exists {verified_string};"
 
     # Test that the function is called with the schema name
-    def test
+    def test_create_schema(schema_name: str):
+        with self.SH.db_session_scope() as scope:
+            scope.execute(f'CREATE SCHEMA IF NOT EXISTS {schema_name}')
+
