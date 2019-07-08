ded3ed50de97c1e8ea7714cd7bd83283bdceaf05
Justin T. Lewis <lewisjt@JTL-MAC.local>
2019-07-08 13:59:20 -0400

    more attempts at mocking the session helper to test the create schema function

--- s3parq/schema_creator.py
+++ s3parq/schema_creator.py
@@ -12,5 +12,6 @@ def schema_generator(schema_name: str):
 
 def create_schema(schema_name: str, session_helper: SessionHelper):
     schema_n = schema_generator(schema_name)
+    session_helper.configure_session_helper()
     with session_helper.db_session_scope() as scope:
         scope.execute(f'CREATE SCHEMA IF NOT EXISTS {schema_n}')
--- tests/test_schema_creator.py
+++ tests/test_schema_creator.py
@@ -3,9 +3,18 @@ import boto3
 from s3parq.session_helper import SessionHelper
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from moto import mock_redshift
-from unittest.mock import patch
+from moto import mock_redshift, mock_iam
+from unittest import mock
+from mock import patch
 from s3parq.schema_creator import schema_generator, create_schema
+
+class MockScopeObj():
+
+    def execute(self, schema_string: str):
+        print(schema_string)
+
+def scope_execute_mock(mock_session_helper):
+        return MockScopeObj()  
 
 @mock_redshift
 class Test():
@@ -16,18 +25,29 @@ class Test():
         assert schema_generator(schema_name) == f"create schema if not exists {schema_name};"
 
     # Test that the function is called with the schema name
-    def test_create_schema(self):
+    @patch('s3parq.schema_creator.SessionHelper')
+    @patch('tests.test_schema_creator.scope_execute_mock')
+    def test_create_schema(self, mock_session_helper, mock_execute):
 
-        mock_session_helper = SessionHelper(
-            'region',
-            'cluster',
-            'host',
-            'port',
-            'dbname'
-            )
-        mock_session_helper.configure_session_helper()
+        # mock_session_helper = msh(
+        #     'region',
+        #     'cluster',
+        #     'host',
+        #     'port',
+        #     'dbname'
+        #     )
+        # mock_session_helper.configure_session_helper()
+        print(mock_session_helper.db_session_scope.return_value)
+        # mock_execute = scope_execute_mock
+        mock_execute.return_value = MockScopeObj()
+        mock_session_helper.db_session_scope.return_value.__enter__ = scope_execute_mock
+
 
         schema_name = "my_string"
-        create_schema(schema_name, mock_session_helper)
-        with mock_session_helper.db_session_scope() as scope:
-            assert scope.execute.called_once_with(f'CREATE SCHEMA IF NOT EXISTS {schema_name}')
+        # with mock_session_helper.db_session_scope() as scope:
+        # with mock.patch('mock_session_helper.db_session_scope') as mock_scope:
+       
+
+        with mock_session_helper.db_session_scope as mock_scope:
+            create_schema(schema_name, mock_session_helper)
+            mock_scope.execute.assert_called_once_with(f'CREATE SCHEMA IF NOT EXISTS {schema_name}')
