e8706d22d4c35299d64a7aa95721c79b1a04891b
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-07-09 14:14:21 -0400

    Added logging for schema creation

--- s3parq/schema_creator.py
+++ s3parq/schema_creator.py
@@ -1,6 +1,9 @@
 from s3parq.session_helper import SessionHelper
 from sqlalchemy import Column, Integer, String
 from s3parq.redshift_naming_helper import RedshiftNamingHelper
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def schema_name_validator(schema_name: str):
@@ -12,4 +15,6 @@ def schema_name_validator(schema_name: str):
 def create_schema(schema_name: str, session_helper: SessionHelper):
     schema_name_validator(schema_name)
     with session_helper.db_session_scope() as scope:
-        scope.execute(f'CREATE SCHEMA IF NOT EXISTS {schema_name}')
+        new_schema_query = f'CREATE SCHEMA IF NOT EXISTS {schema_name}'
+        logger.info(f'Running query to create schema: {new_schema_query}')
+        scope.execute(new_schema_query)
