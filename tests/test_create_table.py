ecd1d8053fe1dd905887fdadf8eee52c68f73594
Justin T. Lewis <lewisjt@JTL-MAC.local>
2019-07-10 14:58:22 -0400

    added basic validation test for table name

---
+++ tests/test_create_table.py
@@ -0,0 +1,12 @@
+from s3parq.create_table import create_table
+
+class Test():
+        
+    # Given a correctly formatted string make sure output is correct SQL
+    def test_validator(self):
+        table_name_good = "my_string"
+        table_name_bad = "my string"
+        table_name_validator(table_name_good)
+        with pytest.raises(ValueError):
+            schema_name_validator(table_name_bad)
+    
