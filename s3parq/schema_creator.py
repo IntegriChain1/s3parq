2eb474815b9e971d143ff18de2afcf6048eaa71b
Justin T. Lewis <lewisjt@JTL-MAC.local>
2019-07-12 10:24:38 -0400

    changed the tests to reflect the proper arguments being passed

--- s3parq/schema_creator.py
+++ s3parq/schema_creator.py
@@ -6,15 +6,28 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def schema_name_validator(schema_name: str):
-    name_validated = RedshiftNamingHelper().validate_name(schema_name)
-    if not name_validated[0]:
-        raise ValueError(name_validated[1])
+def schema_name_validator(schema_name: str, database: str):
+    schema_validated = RedshiftNamingHelper().validate_name(schema_name)
+    database_validated = RedshiftNamingHelper().validate_name(database)
+    if not schema_validated[0] and not database_validated[0]:
+        raise ValueError(schema_validated[1], database_validated[0])
 
 
-def create_schema(schema_name: str, session_helper: SessionHelper):
-    schema_name_validator(schema_name)
+
+def create_schema(schema_name: str, database: str, iam_role: str, session_helper: SessionHelper):
+    schema_name_validator(schema_name, database)
     with session_helper.db_session_scope() as scope:
-        new_schema_query = f'CREATE SCHEMA IF NOT EXISTS {schema_name}'
+        new_schema_query = f"CREATE EXTERNAL SCHEMA IF NOT EXISTS {schema_name} \
+                FROM DATA CATALOG \
+                database '{database}' \
+                iam_role '{iam_role}' \
+                CREATE EXTERNAL DATABASE IF NOT EXISTS;"
+
         logger.info(f'Running query to create schema: {new_schema_query}')
         scope.execute(new_schema_query)
+
+
+
+#     query = f"ALTER TABLE {schema}.{table} \
+#               ADD PARTITION ({' ,'.join(formatted_partitions)}) \
+#               LOCATION 's3://{bucket}/{path_to_data}';"
