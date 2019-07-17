a771bd185b0bfa6084d64dbb9e4e090b94b22aae
natie <njb@integrichain.com>
2019-07-17 10:40:00 -0400

    fixing reserved name functions

--- s3parq/publish_parq.py
+++ s3parq/publish_parq.py
@@ -28,8 +28,7 @@ def check_dataframe_for_timedelta(dataframe: pd.DataFrame)->None:
 
 def _check_partition_compatibility(partition: str) -> bool:
     """ Make sure each partition value is hive-allowed."""
-    reserved = "ALL, ALTER, AND, ARRAY, AS, AUTHORIZATION, BETWEEN, BIGINT, BINARY, BOOLEAN, BOTH, BY, CASE, CAST, CHAR, COLUMN, CONF, CREATE, CROSS, CUBE, CURRENT, CURRENT_DATE, CURRENT_TIMESTAMP, CURSOR, DATABASE, DATE, DECIMAL, DELETE, DESCRIBE, DISTINCT, DOUBLE, DROP, ELSE, END, EXCHANGE, EXISTS, EXTENDED, EXTERNAL, FALSE, FETCH, FLOAT, FOLLOWING, FOR, FROM, FULL, FUNCTION, GRANT, GROUP, GROUPING, HAVING, IF, IMPORT, IN, INNER, INSERT, INT, INTERSECT, INTERVAL, INTO, IS, JOIN, LATERAL, LEFT, LESS, LIKE, LOCAL, MACRO, MAP, MORE, NONE, NOT, NULL, OF, ON, OR, ORDER, OUT, OUTER, OVER, PARTIALSCAN, PARTITION, PERCENT, PRECEDING, PRESERVE, PROCEDURE, RANGE, READS, REDUCE, REVOKE, RIGHT, ROLLUP, ROW, ROWS, SELECT, SET, SMALLINT, TABLE, TABLESAMPLE, THEN, TIMESTAMP, TO, TRANSFORM, TRIGGER, TRUE, TRUNCATE, UNBOUNDED, UNION, UNIQUEJOIN, UPDATE, USER, USING, UTC_TMESTAMP, VALUES, VARCHAR, WHEN, WHERE, WINDOW, WITH, COMMIT, ONLY, REGEXP, RLIKE, ROLLBACK, START, CACHE, CONSTRAINT, FOREIGN, PRIMARY, REFERENCES, DAYOFWEEK, EXTRACT, FLOOR, INTEGER, PRECISION, VIEWS, TIME, NUMERIC, SYNC".split()
-    reserved = [x.strip(',') for x in reserved]
+    reserved = "ALL ALTER AND ARRAY AS AUTHORIZATION BETWEEN BIGINT BINARY BOOLEAN BOTH BY CASE CAST CHAR COLUMN CONF CREATE CROSS CUBE CURRENT CURRENT_DATE CURRENT_TIMESTAMP CURSOR DATABASE DATE DECIMAL DELETE DESCRIBE DISTINCT DOUBLE DROP ELSE END EXCHANGE EXISTS EXTENDED EXTERNAL FALSE FETCH FLOAT FOLLOWING FOR FROM FULL FUNCTION GRANT GROUP GROUPING HAVING IF IMPORT IN INNER INSERT INT INTERSECT INTERVAL INTO IS JOIN LATERAL LEFT LESS LIKE LOCAL MACRO MAP MORE NONE NOT NULL OF ON OR ORDER OUT OUTER OVER PARTIALSCAN PARTITION PERCENT PRECEDING PRESERVE PROCEDURE RANGE READS REDUCE REVOKE RIGHT ROLLUP ROW ROWS SELECT SET SMALLINT TABLE TABLESAMPLE THEN TIMESTAMP TO TRANSFORM TRIGGER TRUE TRUNCATE UNBOUNDED UNION UNIQUEJOIN UPDATE USER USING UTC_TMESTAMP VALUES VARCHAR WHEN WHERE WINDOW WITH COMMIT ONLY REGEXP RLIKE ROLLBACK START CACHE CONSTRAINT FOREIGN PRIMARY REFERENCES DAYOFWEEK EXTRACT FLOOR INTEGER PRECISION VIEWS TIME NUMERIC SYNC".split()
     return not (partition.upper() in reserved)
 
 def check_partitions(partitions: iter, dataframe: pd.DataFrame)->None:
@@ -63,133 +62,6 @@ def check_redshift_params(redshift_params: list):
 def s3_url(bucket: str, key: str):
     return '/'.join(["s3:/", bucket, key])
 
-
-def _get_partitions_for_spectrum(filename: str) -> [str]:
-    '''
-    Turns S3 filepath with partitions into list of only those partitions as strings
-    Args:
-        filename (str): entire filepath for a single file
-    ----
-    Returns:
-        final_partitions (list of strings): these are the partitions for that file
-    --------
-    Example:
-        Args:
-            filename = 'some-path/to/data/zipcode=12345/birth_month=january/final_data_set.parquet
-        ----
-        Returns:
-            final_partitions = ['zipcode=12345', 'birth_month=january']
-    '''
-    filepath = filename.split('/')
-    final_partitions = [_dir for _dir in filepath if '=' in _dir]
-    return final_partitions
-
-
-def _format_partition_strings_for_sql(partitions: [str]) -> [str]:
-    '''
-    Formats a list of S3 partition strings for SQL statements
-    Args:
-        partitions ([str]): list of strings representing the partitions in S3
-    ----
-    Returns:
-        formatted_partitions ([str]): list of the same partitions with quotes
-        to use for SQL
-    --------
-    Example:
-        Args:
-            partitions = ['hamburger=abcd', 'hot_dog=1234']
-        ----
-        Returns:
-            formatted_partitions = ["hamburger='abcd'", "hot_dog='1234'"]
-    '''
-    formatted_partitions = []
-    for p in partitions:
-        key, value = p.split('=')
-        value = f"'{value}'"
-        formatted_partitions.append(f'{key}={value}')
-    return formatted_partitions
-
-
-def last_index_containing_substring(the_list: [str], substring: str) -> int:
-    '''
-    Returns index of last string that contains a substring within a list.  If there's no match, it returns
-    the length of the list + 1 because we want the function that uses it "_get_partition_location"
-    to not identify any partitions if none exists
-    Args:
-        the_list ([str]): list of strings, probably representing the partitions
-        substring (str): string to look for in the_list
-    ----
-    Returns:
-        i (int): The index in the list that contains the first instance of the substring
-        returns -1 if no matches
-    --------
-    Example:
-        Args: 
-            the_list = ['path', 'to', 'data', 'hamburger=abcd', 'hot_dog=1234', 'the_file.parquet]
-            substring = '='
-        ----
-        Returns:
-            i = 3
-        
-    '''
-    for s in reversed(the_list):
-        if substring in s:
-            return len(the_list) - the_list.index(s)
-    return len(the_list) + 1
-
-
-def _get_partition_location(filepath: str):
-    '''
-    Gets location of data in S3 for partitioning in S3.  You need to know the path to 
-    the first partition in order to make a proper Spectrum partition w/ SQL-Redshift
-    Args:
-        filepath (str): path to a file in S3 that is partitioned
-    Returns:
-        final_path (str): path within S3 bucket that has the last partition of a filepath
-    Example:
-        Args: 
-            filepath = 'path/to/data/hamburger=abcd/hot_dog=1234/abcd1234.parquet'
-        Returns:
-            final_path = 'path/to/data/hamburger=abcd/hot_dog=1234'
-    '''
-    separate_dirs = filepath.split('/')
-    last_partition = last_index_containing_substring(separate_dirs, "=")
-    final_set = separate_dirs[:-last_partition + 1] #  I think this is more confusing than it has to be
-    final_path = '/'.join(final_set)
-    if final_path == '':
-        raise ValueError(f'No partitions in this filepath {filepath}')
-    return final_path
-
-
-def _generate_partition_sql(bucket: str, schema: str, table: str, filepath: str) -> str:
-    '''
-    Generates partitioning SQL
-    Args:
-        bucket (str): S3 bucket where data is stored
-        schema (str): name of redshift schema (must already exist)
-        table (str): name of table in schema.  Must have partitions scoped out in `CREATE TABLE ...`
-        filepath (str): path to data in S3 that will be queryable by it's partitions
-    ----
-    Returns:
-        query (str): a raw SQL string that adds the partitioned data to the table
-    --------
-    Example:
-        Args:
-            bucket = 'MyBucket'
-            schema = 'MySchema'
-            table = 'MyTable'
-            filepath = 'path/to/data/apple=abcd/banana=1234/abcd1234.parquet'
-        Returns:
-            "ALTER TABLE MySchema.MyTable               ADD PARTITION (apple='abcd' ,banana='1234')               LOCATION 's3://MyBucket/path/to/data/apple=abcd';"
-    '''
-    partitions = _get_partitions_for_spectrum(filepath)
-    formatted_partitions = _format_partition_strings_for_sql(partitions)
-    path_to_data = _get_partition_location(filepath)
-    query = f"ALTER TABLE {schema}.{table} \
-              ADD PARTITION ({' ,'.join(formatted_partitions)}) \
-              LOCATION 's3://{bucket}/{path_to_data}';"
-    return query
-
 def _gen_parquet_to_s3(bucket: str, key: str, dataframe: pd.DataFrame,
                        partitions: list) -> None:
     """ pushes the parquet dataset directly to s3. """
@@ -203,7 +75,7 @@ def _gen_parquet_to_s3(bucket: str, key: str, dataframe: pd.DataFrame,
     logger.debug("Done writing to location.")
 
 
-def _assign_partition_meta(bucket: str, key: str, dataframe: pd.DataFrame, partitions: iter, write_to_redshift: bool = False) -> List[str]:
+def _assign_partition_meta(bucket: str, key: str, dataframe: pd.DataFrame, partitions: iter, session_helper: SessionHelper) -> List[str]:
     """ assigns the dataset partition meta to all keys in the dataset"""
     s3_client = boto3.client('s3')
     all_files_without_meta = []
@@ -216,10 +88,7 @@ def _assign_partition_meta(bucket: str, key: str, dataframe: pd.DataFrame, parti
                 if not 'partition_data_types' in head_obj['Metadata']:
                     all_files_without_meta.append(obj['Key'])
                 if write_to_redshift:
-                    sql_command = _generate_partition_sql(bucket, 'spectrum', 'test12', obj['Key'])
-                    print(sql_command)
-                    # with SH.db_session_scope() as scope:
-                    #     scope.execute(sql_command)
+                    sql_command = publish_redshift.create_partitions(bucket, 'spectrum', 'test12', obj['Key'], session_helper)
     
 
     for obj in all_files_without_meta:
@@ -329,6 +198,28 @@ def publish(bucket: str, key: str, partitions: iter, dataframe: pd.DataFrame, re
             port: str 
             db_name: str
     """
+    session_helper = None
+
+    if redshift_params:
+        logger.debug("Found redshift parameters. Checking validity of params...")
+        check_redshift_params(redshift_params)
+        logger.debug("Redshift parameters valid. Opening Session helper.")
+        session_helper = SessionHelper(
+            region = redshift_params['region'],
+            cluster_id = redshift_params['cluster'],
+            host = redshift_params['host'],
+            port = redshift_params['port'],
+            db_name = redshift_params['db_name']
+        )
+        session_helper.configure_session_helper()
+        publish_redshift.create_schema(redshift_params['schema_name'], redshift_params['db_name'], redshift_params['iam_role'], session_helper)
+        logger.debug(f"Schema {redshift_params['schema_name']} created. Creating table {redshift_params['table_name']}...")
+
+        df_types = _get_dataframe_datatypes(dataframe, partitions)
+        partition_types = _get_dataframe_datatypes(dataframe, partitions, True)
+        publish_redshift.create_table(redshift_params['table_name'], redshift_params['schema_name'], df_types, partition_types, s3_url(bucket, key), session_helper)
+        logger.debug(f"Table {redshift_params['table_name']} created.")
+
     logger.info("Checking params...")
     check_empty_dataframe(dataframe)
     check_dataframe_for_timedelta(dataframe)
@@ -347,28 +238,9 @@ def publish(bucket: str, key: str, partitions: iter, dataframe: pd.DataFrame, re
                                                  key=key,
                                                  dataframe=dataframe,
                                                  partitions=partitions,
-                                                 write_to_redshift=True)
+                                                 session_helper=session_helper)
         files = files + published_files
 
     logger.debug("Done writing to S3.")
-
-    if redshift_params:
-        check_redshift_params(redshift_params)
-        logger.debug("Opening Session helper.")
-        session_helper = SessionHelper(
-            region = redshift_params['region'],
-            cluster_id = redshift_params['cluster'],
-            host = redshift_params['host'],
-            port = redshift_params['port'],
-            db_name = redshift_params['db_name']
-        )
-        session_helper.configure_session_helper()
-        publish_redshift.create_schema(redshift_params['schema_name'], redshift_params['db_name'], redshift_params['iam_role'], session_helper)
-        logger.debug(f"Schema {redshift_params['schema_name']} created. Creating table {redshift_params['table_name']}...")
-
-        df_types = _get_dataframe_datatypes(dataframe, partitions)
-        partition_types = _get_dataframe_datatypes(dataframe, partitions, True)
-        publish_redshift.create_table(redshift_params['table_name'], redshift_params['schema_name'], df_types, partition_types, s3_url(bucket, key), session_helper)
-        logger.debug(f"Table {redshift_params['table_name']} created.")
 
     return files
