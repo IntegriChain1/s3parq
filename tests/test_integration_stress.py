4d61d750dd71c6b7b97ddc5631032078cf1d5190
schafrn <rns@integrichain.com>
2019-10-07 12:04:53 -0400

    uncommented accidentally commented unit tests :disappointed:

--- tests/test_integration_stress.py
+++ tests/test_integration_stress.py
@@ -1,48 +1,48 @@
-# import boto3
-# import moto
-# import s3parq
-# import pytest
-# import dfmock
-# from s3parq.publish_parq import publish
-# from s3parq.fetch_parq import fetch
-# import pandas as pd
+import boto3
+import moto
+import s3parq
+import pytest
+import dfmock
+from s3parq.publish_parq import publish
+from s3parq.fetch_parq import fetch
+import pandas as pd
 
 
-# @moto.mock_s3
-# def test_end_to_end():
-#     df = dfmock.DFMock(count=1000)
-#     df.columns = {"string_options": {"option_count": 4, "option_type": "string"},
-#                 "int_options": {"option_count": 4, "option_type": "int"},
-#                 "datetime_options": {"option_count": 5, "option_type": "datetime"},
-#                 "float_options": {"option_count": 2, "option_type": "float"},
-#                 "metrics": "integer"
-#                 }
-#     df.generate_dataframe()
-#     df.grow_dataframe_to_size(250)
+@moto.mock_s3
+def test_end_to_end():
+    df = dfmock.DFMock(count=1000)
+    df.columns = {"string_options": {"option_count": 4, "option_type": "string"},
+                "int_options": {"option_count": 4, "option_type": "int"},
+                "datetime_options": {"option_count": 5, "option_type": "datetime"},
+                "float_options": {"option_count": 2, "option_type": "float"},
+                "metrics": "integer"
+                }
+    df.generate_dataframe()
+    df.grow_dataframe_to_size(250)
     
-#     s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3')
 
-#     bucket_name = 'thistestbucket'
-#     key = 'thisdataset'
+    bucket_name = 'thistestbucket'
+    key = 'thisdataset'
 
-#     s3_client.create_bucket(Bucket=bucket_name)
+    s3_client.create_bucket(Bucket=bucket_name)
 
-#     # pub it
-#     publish(
-#         bucket=bucket_name,
-#         key=key,
-#         dataframe=df.dataframe,
-#         partitions=['string_options', 'datetime_options', 'float_options']
-#     )
+    # pub it
+    publish(
+        bucket=bucket_name,
+        key=key,
+        dataframe=df.dataframe,
+        partitions=['string_options', 'datetime_options', 'float_options']
+    )
 
-#     # go get it
-#     fetched_df = fetch(
-#         bucket=bucket_name,
-#         key=key,
-#         parallel=False
-#     )
+    # go get it
+    fetched_df = fetch(
+        bucket=bucket_name,
+        key=key,
+        parallel=False
+    )
 
-#     assert fetched_df.shape == df.dataframe.shape
-#     pd.DataFrame.eq(fetched_df, df.dataframe)
-#     fetched_df.head()
+    assert fetched_df.shape == df.dataframe.shape
+    pd.DataFrame.eq(fetched_df, df.dataframe)
+    fetched_df.head()
 
