a6e14413b9067dcb1183c656f4791c599e5a0ad3
Jeremy Lecoeur <jlecoeur@integrichain.com>
2022-04-07 15:55:28 -0400

    simpler code for data enforcing

--- s3parq/publish_parq.py
+++ s3parq/publish_parq.py
@@ -328,10 +328,10 @@ def _parquet_schema(dataframe: pd.DataFrame, custom_redshift_columns: dict = Non
         elif dtype.startswith('int8'):
             pa_type = pa.int8()
         elif dtype.startswith('Int32'):
-            dataframe[col] = dataframe[col].astype({col: 'object'})
+            dataframe = dataframe.astype({col: 'object'})
             pa_type = pa.int32()
         elif dtype.startswith('Int64'):
-            dataframe[col] = dataframe[col].astype({col: 'object'})
+            dataframe = dataframe.astype({col: 'object'})
             pa_type = pa.int64()
         elif dtype.startswith('float32'):
             pa_type = pa.float32()
